dfac23d107847a19428e97d7a4bec7485af826d2
Andreas Furster <andreas.furster@hotmail.com>
2020-10-14 21:53:38 +0200

    Seperating init

---
+++ application/hyperleger-fabric/AppUtil.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const appDir = path.dirname(require.main.filename);
+
+exports.buildCCPOrg1 = () => {
+	// load the common connection configuration file
+	const ccpPath = path.resolve(appDir, process.env.ORG1_CONNECTION_CONFIG);
+	const fileExists = fs.existsSync(ccpPath);
+	if (!fileExists) {
+		throw new Error(`no such file or directory: ${ccpPath}`);
+	}
+	const contents = fs.readFileSync(ccpPath, 'utf8');
+
+	// build a JSON object from the file contents
+	const ccp = JSON.parse(contents);
+
+	console.log(`Loaded the network configuration located at ${ccpPath}`);
+	return ccp;
+};
+
+exports.buildCCPOrg2 = () => {
+	// load the common connection configuration file
+	const ccpPath = path.resolve(__dirname,  process.env.ORG2_CONNECTION_CONFIG);
+	const fileExists = fs.existsSync(ccpPath);
+	if (!fileExists) {
+		throw new Error(`no such file or directory: ${ccpPath}`);
+	}
+	const contents = fs.readFileSync(ccpPath, 'utf8');
+
+	// build a JSON object from the file contents
+	const ccp = JSON.parse(contents);
+
+	console.log(`Loaded the network configuration located at ${ccpPath}`);
+	return ccp;
+};
+
+exports.buildWallet = async (Wallets, walletPath) => {
+	// Create a new  wallet : Note that wallet is for managing identities.
+	let wallet;
+	if (walletPath) {
+		wallet = await Wallets.newFileSystemWallet(walletPath);
+		console.log(`Built a file system wallet at ${walletPath}`);
+	} else {
+		wallet = await Wallets.newInMemoryWallet();
+		console.log('Built an in memory wallet');
+	}
+
+	return wallet;
+};
+
+exports.prettyJSONString = (inputString) => {
+	if (inputString) {
+		 return JSON.stringify(JSON.parse(inputString), null, 2);
+	}
+	else {
+		 return inputString;
+	}
+};
